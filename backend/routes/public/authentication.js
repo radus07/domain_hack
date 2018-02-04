a2451e85506ef6f2558eb5eb9418646235ada4c1
Radu Balan <radu.balan.97@gmail.com>
2018-02-04 08:11:53 +0200

    Backend: fix response of auth api endpoint

--- backend/routes/public/authentication.js
+++ backend/routes/public/authentication.js
@@ -16,14 +16,14 @@ module.exports = (app) => {
       username: credentials.username,
       password: credentials.password
     }, (err, result) => {
-      if (err || result.length === 0) res.json({status: 404})
+      if (err || !result) res.json({status: 404})
       else {
         const token = jwt.sign(
           {user_id: result._id},
           CONFIG.S_W,
           {expiresIn: 86400} /* 24 hours */
         )
-        res.json(token)
+        res.json({status: 200, data: token})
       }
     })
   })
