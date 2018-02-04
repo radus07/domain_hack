40ace71d7739f3b431e1236e25af800459bfb49d
Radu Balan <radu.balan.97@gmail.com>
2018-02-04 12:29:06 +0200

    Backend: create api endpoint to manage users entity

--- backend/routes/protected/index.js
+++ backend/routes/protected/index.js
@@ -2,4 +2,5 @@ module.exports = (app) => {
   /**
    * Add all protected routes here
    */
+  require('./users')(app)
 }
---
+++ backend/routes/protected/users.js
@@ -0,0 +1,38 @@
+const DB = require('../../config/config')
+const mongoose = require('mongoose')
+const User = require('../../model/user')
+
+mongoose.connect(DB.DB_URL + DB.DB_NAME)
+
+module.exports = (app) => {
+  app.get('/api/users', (req, res) => {
+    User.find({}, (err, result) => {
+      if (err || result.length === 0) res.json({status: 404})
+      else res.json({status: 200, data: result})
+    })
+  })
+  app.post('/api/users', (req, res) => {
+    const user = new User({
+      username: req.body.username,
+      password: Buffer.from(req.body.password).toString('base64')
+    })
+    user.save((err) => {
+      if (err) res.json({status: 403})
+      else res.json({status: 201})
+    })
+  })
+  app.put('/api/users', (req, res) => {
+    User.update({_id: req.body.id}, {username: req.body.username}, {upsert: true}, (err) => {
+      if (err) res.json({status: 403})
+      else res.json({status: 200})
+    })
+  })
+  app.delete('/api/users', (req, res) => {
+    req.body.users.forEach(user => {
+      User.findByIdAndRemove(user._id, (err) => {
+        if (err) res.json({status: 403})
+      })
+    })
+    res.json({status: 200})
+  })
+}
