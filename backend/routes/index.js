38d38effcb5eafab151bff77a8e2575bd0f0b7a3
Radu Balan <radu.balan.97@gmail.com>
2018-02-04 14:00:06 +0200

    Backend: restrict access to protected routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,13 +1,33 @@
+const jwt = require('jsonwebtoken')
+const CONFIG = require('../config/config')
+
 module.exports = (app) => {
   /**
    * Public routes
    */
   require('./public')(app)
 
-  // TODO add permissions validation
+  app.use((req, res, next) => {
+    const token = req.headers['authorization']
+    validateToken(req, res, next, token)
+  })
 
   /**
    * Protected routes
    */
   require('./protected')(app)
+}
+
+function validateToken (req, res, next, token) {
+  if (token && token.split(' ')[0] === 'Bearer') {
+    jwt.verify(token.split(' ')[1], CONFIG.S_W, (err, decoded) => {
+      if (err) {
+        return res.json(401)
+      }
+      req.decoded = decoded
+      next()
+    })
+  } else {
+    return res.json(401)
+  }
 }
